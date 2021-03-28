ab23d4fd91b9e05a5f83a177b529feb02e60554a
Colin Jalbert <colin.jalbert1@gmail.com>
2021-03-27 18:24:44 -0700

    Enter

---
+++ Canvas/canvas.js
@@ -0,0 +1,95 @@
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height =window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+//Rectangle
+c.fillStyle = 'rgba(255, 100, 90, 0.5)';
+c.fillRect(100, 100, 100, 100)
+c.fillStyle = 'rgba(5, 200, 90, 0.5)';
+c.fillRect(400, 100, 100, 100)
+c.fillStyle = "black";
+c.fillRect(300, 300, 100, 100)
+console.log(canvas);
+
+// Line
+// c.beginPath();
+// c.moveTo(50,300);
+// c.lineTo(300,100);
+// c.lineTo(400,300);
+// c.lineTo(400,200);
+// c.lineTo(300,200);
+// c.lineTo(50,300);
+// c.strokeStyle = "Blue";
+// c.stroke();
+
+//Arc / Circle
+// c.beginPath();
+// c.arc(300, 300, 30, Math.PI * 2, false);
+// c.strokeStyle = 'blue';
+// c.stroke();
+
+// for (var i = 0; i < 3; i++){
+//     c.beginPath();
+//     c.arc(300, 300 + i * 20, 30, Math.PI * 2, false);
+//     c.strokeStyle = 'blue';
+//     c.stroke();
+// }
+
+// for (var i = 0; i < 100; i++){
+//    var x = Math.random() * window.innerWidth;
+//    var y = Math.random() * window.innerHeight;
+//     c.beginPath();
+//     c.arc(x, y, 30, Math.PI * 2, false);
+//     c.strokeStyle = 'blue';
+//     c.stroke();
+// }
+
+
+
+function Circle(x, y, dx, dy, radius){
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius;
+
+        this.draw = function(){
+            c.beginPath();
+            c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            //c.strokeStyle = 'blue';
+            c.fillStyle = "pink";
+            c.fill();
+            c.stroke();
+        }
+        this.update = function(){
+            if (this.x + this.radius > innerWidth || this.x -this.radius < 0){this.dx = -this.dx;}
+            if (this.y + this.radius > innerHeight || this.y -this.radius < 0) {this.dy = -this.dy;}
+            this.x += this.dx;
+            this.y += this.dy;
+
+            this.draw();
+        }
+    }
+
+var circleArray = [];
+
+for (var i = 0; i < 100; i++){
+    var radius = 30;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5) * 8;
+    var dy = (Math.random() - 0.5) * 8;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+
+    for (var i = 0; i < circleArray.length; i ++){
+        circleArray[i].update();
+    }
+}
+    animate();
