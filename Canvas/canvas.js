4eb1bd183b65c8f61eced4a3805936e56775a985
Colin Jalbert <colin.jalbert1@gmail.com>
2021-03-27 18:26:30 -0700

    color to black

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -59,7 +59,7 @@ function Circle(x, y, dx, dy, radius){
             c.beginPath();
             c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
             //c.strokeStyle = 'blue';
-            c.fillStyle = "pink";
+            c.fillStyle = "black";
             c.fill();
             c.stroke();
         }
