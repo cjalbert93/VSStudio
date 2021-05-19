137f6fc319bfbe6f69ad5646d2139dd8ce15ea8e
cjalbert93 <colin.jalbert1@gmail.com>
2021-05-19 17:30:58 -0400

    made a ton
    websites

---
+++ Trainset/canvas.js
@@ -0,0 +1,78 @@
+var canvas = document.querySelector('canvas');
+
+
+var c = canvas.getContext('2d');
+
+//Rectangle
+c.fillStyle = "white";
+c.fillRect(0, 0, 1400, 700)
+c.fillStyle = "black"
+c.fillRect(40, 40, 10, 10 )
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 40, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 80, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 120, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 160, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 200, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 240, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 280, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 320, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 360, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 400, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 440, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 480, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 520, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 560, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 600, 10, 10)
+c.fillStyle = "black";
+}
+for (var i = 0; i <34; i++){
+    c.fillRect(40+40*i, 640, 10, 10)
+c.fillStyle = "black";
+}
+
+
+
+console.log(canvas);
